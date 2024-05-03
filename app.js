89d88c7f027ab488e6bcb181191f46e09b641c90
VireshMeghanathi <123720255+VireshMeghanathi@users.noreply.github.com>
2024-05-03 18:08:49 +0530

    level 2 authentaction and encription done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -20,6 +21,9 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
+
+const secret = "thisisourlittlesecret.";
+userSchema.plugin(encrypt,{secret:secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
