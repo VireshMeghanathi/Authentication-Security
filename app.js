4844af3d933a65cc110cc40070e8d068fd025254
VireshMeghanathi <123720255+VireshMeghanathi@users.noreply.github.com>
2024-05-03 15:21:03 +0530

    leval 1 authentacation complet

--- app.js
+++ app.js
@@ -37,8 +37,36 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+    .then(() => {
+        res.render("secrets");
+      })
+     .catch((err) => {
+          res.status(500).send("Internal server error");
+     })
+});
+
+app.post("/login",function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+    .then((user) => {
+        if(user.password === password){
+            res.render("secrets");
+        }
+    })
+    .catch((err) => {
+        res.status(500).send("Internal server error");
+    })
     
-})
+});
+    
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
